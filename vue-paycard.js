cb7c0bf89be80caa2748014298beb61cb3dd6329
Igor Guastalla <limaguastallaigor@gmail.com>
2021-01-17 17:17:02 -0300

    chore: modify installation component method. update readme

--- vue-paycard.js
+++ vue-paycard.js
@@ -1,13 +1,39 @@
-import VuePaycard from './src/components/VuePaycard'
+// Import vue component
+import VuePaycard from './src/components/VuePaycard.vue'
 
-const VuePaycardPlugin = {
-  install (Vue) {
-    Vue.component('VuePaycard', VuePaycard)
-  }
+// install function executed by Vue.use()
+const install = function (Vue) {
+  if (install.installed) return
+  install.installed = true
+  Vue.component('VuePaycard', VuePaycard)
 }
 
-// Export as a plugin
-export default VuePaycardPlugin
+// Create module definition for Vue.use()
+const plugin = {
+  install
+}
 
-// Export as individual components
+// To auto-install when vue is found
+// eslint-disable-next-line no-redeclare
+/* global window, global */
+let GlobalVue = null
+
+if (typeof window !== 'undefined') {
+  GlobalVue = window.Vue
+} else if (typeof global !== 'undefined') {
+  GlobalVue = global.Vue
+}
+
+if (GlobalVue) {
+  GlobalVue.use(plugin)
+}
+
+// Inject install function into component - allows component
+// to be registered via Vue.use() as well as Vue.component()
+VuePaycard.install = install
+
+// Export component by default
+export default VuePaycard
+
+// Export single (backwards compatibility)
 export { VuePaycard }
